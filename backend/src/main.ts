7c89b141cab73172318cf84a675bea53180e26b5
maria_b <burtseva_ma@mail.ru>
2026-05-14 22:11:47 +0500

    Подготовка к развертыванию проекта

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -14,8 +14,8 @@ async function bootstrap() {
     }),
   );
 
-  const port = Number(process.env['API_PORT'] ?? 3000);
-  await app.listen(port);
+  const port = Number(process.env['PORT'] ?? process.env['API_PORT'] ?? 3000);
+  await app.listen(port, '0.0.0.0');
 }
 
 void bootstrap();
